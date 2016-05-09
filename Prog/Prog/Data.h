b1d0a93175413143a82e0b057b74b89c508c823c
unknown <up201503871@B02AU0724.salas.feupsig.fe.up.pt>
2016-05-09 11:09:03 +0100

    Adicionei a pasta prog e os source e headers

---
+++ Prog/Prog/Data.h
@@ -0,0 +1,67 @@
+# pragma once
+
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+
+class Data {
+ private:
+  int dia;
+  int mes;
+  int ano;
+
+ public:
+  Data(){};
+  Data(string data); // data na forma DD/MM/AAA
+  int getDia() const;
+  int getMes() const;
+  int getAno() const;
+  void setDia(int dia);
+  void setMes(int mes);
+  void setAno(int ano);
+  void save(ofstream & out) const;
+  friend ostream& operator<<(ostream& out, const Data & data);
+};
+
+class Date
+{
+public: // access specifier; users can only access the PUBLIC members
+	Date(); // constructor; constructors have the name of the class
+	Date(unsigned int y, unsigned int m, unsigned int d);
+	Date(string daymonthyear); // constructors can be overloaded
+
+	void setYear(unsigned int y); // member function OR method
+	void setMonth(unsigned int m);
+	void setDay(unsigned int d);
+	void setDate(unsigned int y, unsigned int m, unsigned int d);
+	unsigned int getYear();
+	unsigned int getMonth();
+	unsigned int getDay();
+	unsigned int getTodayDay();
+	unsigned int getTodayMonth();
+	unsigned int getTodayYear();
+	bool isValid();
+	void Validate();
+	bool operator==(const Date& rhs);
+	bool operator>(const Date& rhs);
+	bool operator>=(const Date& rhs);
+	bool isLeapYear(int year);
+	string getStr(); // get (return) date as a string
+	void save(ofstream & out) const;
+	friend ostream& operator<<(ostream& out, const Data & data);
+	void show();
+
+private: // PRIVATE data & function members are hidden from the user
+	unsigned int year; // data member
+	unsigned int month;
+	unsigned int day;
+	bool validDay;
+	bool validMonth;
+	bool validYear;
+	bool valid = isValid();
+};
